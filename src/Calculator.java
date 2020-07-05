67ec24495bb0382c78f67a4a6d8a6fef8f341d84
tinas <tina.soni112@gmail.com>
2020-07-05 15:56:20 +0530

    My First Java Program - Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Calculator {
+    int sum(int a, int b) {
+        int sum = a + b;
+        return sum;
+
+    }
+    int sub(int a, int b){
+        int sub = a-b;
+        return sub;
+    }
+    int mul(int a, int b){
+        int mul = a*b;
+        return mul;
+    }
+    float div(int a ,int b){
+        float div = a/b;
+        return div;
+    }
+   public static void main(String[] args){
+
+
+
+
+
+
+   }
+}
